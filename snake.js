7247444f3c8599d458d304179735f6dccd94a260
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2026-08-22 23:45:30 +0000

    b

--- snake.js
+++ snake.js
@@ -12,49 +12,15 @@ var lastspacekey = false;
 
 function handleKeyDown(event){
   lastspacekey = spacekey;
-  lastmenu = menu;
   // controls
-  if (event.key === ' ' || event.key === 'ArrowUp' || event.key === 'w') {
+  if (event.key === ' ') {
     spacekey = true;
-    if(menu && lastmenu){
-      menu = false;
-      Reset();
-    }
-  }
-  
-  // exit etc
-  if (event.key === 'e') {
-    if(!menu){
-      menu = true;
-    }
-  }
-  if (event.key === 'Enter') {
-    if(menu){
-      menu = false;
-      Reset();
-    }
   }
 }
 function handleKeyUp(event){
   // controls
-  if (event.key === ' ' || event.key === 'ArrowUp' || event.key === 'w') {
+  if (event.key === ' ') {
     spacekey = false;
-  }
-  if (event.key === 'n') {
-    if(noclip){
-      noclip = false;
-    }
-    else{
-      noclip = true;
-    }
-  }
-  if (event.key === 'm') {
-    if(macro){
-      macro = false;
-    }
-    else{
-      macro = true;
-    }
   }
 }
 
@@ -65,16 +31,9 @@ function Logic(){
 function Draw(){
   ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  // menu screen
-  if(menu){
-    ctx.fillStyle = 'black';
-    ctx.font = '16px sans-serif';
-    ctx.fillText('Press e to exit', 160, 48);
-  }
-  // game screen
-  else{
-    
-  }
+  ctx.fillStyle = 'black';
+  ctx.font = '16px sans-serif';
+  ctx.fillText('something', 160, 48);
 }
 
 function Update() {
