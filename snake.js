bc3f055e7450782fedbd56cddd380deb86c528fe
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2026-08-22 23:49:38 +0000

    starting snake logic

--- snake.js
+++ snake.js
@@ -1,10 +1,15 @@
-const canvas = document.getElementById('gameCanvas');
+const canvas = document.getElementById('snakeCanvas');
 const ctx = canvas.getContext('2d');
 
 var loop;
 
-const screenw = 500;
-const screenh = 500;
+const screenw = 10;
+const screenh = 10;
+
+const gridSize = 50;
+
+var xpos = 0;
+var ypos = 0;
 
 /*----- Input -----*/
 var spacekey = false;
@@ -34,6 +39,10 @@ function Draw(){
   ctx.fillStyle = 'black';
   ctx.font = '16px sans-serif';
   ctx.fillText('something', 160, 48);
+
+  // head
+  ctx.fillStyle = 'green';
+  ctx.fillRect(xpos, ypos, gridSize, gridSize);
 }
 
 function Update() {
