02a8a0b843fd789f865b4a09d84c28e27c8db15d
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2026-08-23 07:17:13 +0000

    b

--- my3d.js
+++ my3d.js
@@ -6,6 +6,17 @@ import * as THREE from "three";
 const w = canvas.width;
 const h = canvas.height;
 
-const renderer = new THREE.WebGLRenderer({ antialias : true });
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias : true });
 renderer.setSize(w, h);
-document.body.appendChild(renderer.domElement);
+//document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+renderer.render(scene, camera);
