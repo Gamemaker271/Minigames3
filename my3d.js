ab86c54dc764920ef8bfcf83c1c713554410ffb3
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2026-08-23 07:10:40 +0000

    a

--- my3d.js
+++ my3d.js
@@ -2,3 +2,10 @@ const canvas = document.getElementById('my3dCanvas');
 const ctx = canvas.getContext('2d');
 
 import * as THREE from "three";
+
+const w = canvas.width;
+const h = canvas.height;
+
+const renderer = new THREE.WebGLRenderer({ antialias : true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
