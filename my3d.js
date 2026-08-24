a0785ed5f6f5b103fb95c5f6f869dfeb7f5e675c
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2026-08-24 06:54:47 +0000

    b

--- my3d.js
+++ my3d.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('my3dCanvas');
-const ctx = canvas.getContext('2d');
+//const canvas = document.getElementById('my3dCanvas');
+//const ctx = canvas.getContext('2d');
 
 import * as THREE from "three";
 
