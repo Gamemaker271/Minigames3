c8de1e4cc0220b6365e0509f2aa3ee5ddd530327
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2026-08-25 10:21:12 +0000

    2nd 3d site

---
+++ 3dCamera.js
@@ -0,0 +1,45 @@
+const canvas = document.getElementById('3dCameraCanvas');
+//const ctx = canvas.getContext('2d');
+
+import * as THREE from "three";
+
+const w = canvas.width;
+const h = canvas.height;
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias : true });
+renderer.setSize(w, h);
+//document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const mat = new THREE.MeshStandardMaterial({
+    color: 0xccff,
+    flatShading: true
+});
+
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+mesh.add(wireMesh);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
+scene.add(hemiLight);
+
+function animate(t = 0){
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
