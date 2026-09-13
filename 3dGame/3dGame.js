10d728da43680c993e366119a73aada973aa5a4e
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2026-09-13 21:49:40 +0000

    update texture

--- 3dGame/3dGame.js
+++ 3dGame/3dGame.js
@@ -12,7 +12,7 @@ renderer.setSize(w, h);
 /*----- Textures -----*/
 const textureLoader = new THREE.TextureLoader();
 
-var cubeTexture = textureLoader.load('Textures/birdwall3.png');
+var cubeTexture = textureLoader.load('Textures/birdwall2.png');
 cubeTexture.magFilter = THREE.NearestFilter;
 cubeTexture.minFilter = THREE.NearestFilter;
 var wallTexture = textureLoader.load('Textures/wall.png');
@@ -102,7 +102,7 @@ for (let i = 0; i < walls.length; i++){
   if(walls[i].type == 0){
     wallTexture = textureLoader.load('Textures/wall.png');
   } else if (walls[i].type == 1){
-    wallTexture = textureLoader.load('Textures/birdwall3.png');
+    wallTexture = textureLoader.load('Textures/birdwall2.png');
   } else {
     wallTexture = textureLoader.load('Textures/wall.png');
   }
